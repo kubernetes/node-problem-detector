e4a12274b659f4f7843266db4a09ef5e35aa19a6
tongxin21 <tongxin5@jd.com>
2019-10-28 09:43:07 +0800

    add "centos" system id

--- pkg/util/helpers.go
+++ pkg/util/helpers.go
@@ -83,12 +83,14 @@ func GetOSVersion() (string, error) {
 		return "", err
 	}
 	switch osReleaseMap["ID"] {
-	case "centos":
+	case "cos":
 		return getCOSVersion(osReleaseMap), nil
 	case "debian":
 		return getDebianVersion(osReleaseMap), nil
 	case "ubuntu":
 		return getDebianVersion(osReleaseMap), nil
+	case "centos":
+		return getCOSVersion(osReleaseMap), nil
 	default:
 		return "", fmt.Errorf("Unsupported ID in /etc/os-release: %q", osReleaseMap["ID"])
 	}
