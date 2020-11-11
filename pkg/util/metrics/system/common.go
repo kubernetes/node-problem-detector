13b6bc4afa33650869e04b257b293169f7922c66
varsha teratipally <teratipally@google.com>
2020-11-11 21:34:01 +0000

    moved reading files to common class

--- pkg/util/metrics/system/common.go
+++ pkg/util/metrics/system/common.go
@@ -1,1 +1,25 @@
 package system
+
+import (
+	"bufio"
+	"os"
+)
+
+// ReadFile reads contents from a file and returns lines.
+func ReadFile(filename string) ([]string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var result []string
+	s := bufio.NewScanner(file)
+	for s.Scan() {
+		result = append(result, s.Text())
+	}
+	if s.Err() != nil {
+		return nil, err
+	}
+	return result, nil
+}
